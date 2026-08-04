6a8dfb5c1a3c8f2d25d4ccfa6b48cf9b6933b93c
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-03 23:45:33 -0400

    updated errors messages being displayed in the log-in post route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -13,15 +13,24 @@ UsersRouter.get("/log-out", passportController.logoutRequest);
 UsersRouter.post("/sign-up", passportController.encryptPassword);
 UsersRouter.post(
   "/log-in",
-  body("password").isLength({ min: 5 }),
-  body("confirm-password").custom(
-    (value, { req }) => value === req.body.password
-  ),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be at least 5 characters long"),
+  body("confirm-password").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Password confirmation does not match password");
+    }
+    return true;
+  }),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render("failure", {
-        errors: JSON.stringify(errors)
+      const errorMap = errors.array().reduce((acc, error) => {
+        acc[error.path] = error.msg;
+        return acc;
+      }, {});
+      res.status(404).render("failure", {
+        errors: Object.values(errorMap)
       });
     }
     next();
