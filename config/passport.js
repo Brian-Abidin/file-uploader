2ed5fe73eede796763d144359845c2eedc3ec5fc
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-05 16:23:56 -0400

    updated queries to prisma typedsql code

--- config/passport.js
+++ config/passport.js
@@ -3,6 +3,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
 const { hash } = require("node:crypto");
 const pool = require("../db/pool");
+const prisma = require("../lib/prisma");
 
 // example used in odin project -- update for project
 
@@ -10,11 +11,15 @@ const pool = require("../db/pool");
 // acts like a middleware and is called when asking passport for authentication
 const localStratregy = new LocalStrategy(async (username, password, done) => {
   try {
-    const { rows } = await pool.query(
-      "SELECT * FROM users WHERE username = $1",
-      [username]
-    );
-    const user = rows[0];
+    const user = await prisma.User.findUnique({
+      where: { username }
+    });
+    // CODE BELOW IS FOR RAW QUERIES WITHOUT PRISMA
+    // const { rows } = await pool.query(
+    //   "SELECT * FROM users WHERE username = $1",
+    //   [username]
+    // );
+    // const user = rows[0];
     if (!user) {
       return done(null, false, { message: "Incorrect username" });
     }
@@ -38,11 +43,14 @@ const serializeUser = (user, done) => {
 
 const deserializeUser = async (id, done) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id
-    ]);
-    const user = rows[0];
-
+    const user = prisma.User.findUnique({
+      where: { id }
+    });
+    // CODE BELOW IS FOR RAW QUERIES WITHOUT PRISMA
+    // const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+    //   id
+    // ]);
+    // const user = rows[0];
     done(null, user);
   } catch (err) {
     done(err);
