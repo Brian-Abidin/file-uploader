9387654bf2a6fa8e1b496e01f33c91bbd617892d
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-06 12:45:30 -0400

    updated query in encryptPassword to be compatible with prisma

--- config/passport.js
+++ config/passport.js
@@ -81,10 +81,17 @@ const logoutRequest = (req, res, next) => {
 const encryptPassword = async (req, res, next) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-      req.body.username,
-      hashedPassword
-    ]);
+    await prisma.user.create({
+      data: {
+        username: req.body.username,
+        password: hashedPassword
+      }
+    });
+    // CODE BELOW IS FOR QUERY WITHOUT PRISMA
+    // await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+    //   req.body.username,
+    //   hashedPassword
+    // ]);
     res.redirect("/");
   } catch (error) {
     console.error(error);
