b0f200d903a8aba068563a9b33864541dac22ff6
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-12 14:24:42 -0400

    commented out password validation for login and added it to sign up route

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -10,9 +10,8 @@ UsersRouter.get("/", usersController.getIndex);
 UsersRouter.get("/sign-up", usersController.getForm);
 UsersRouter.get("/failure", usersController.getFailure);
 UsersRouter.get("/log-out", passportController.logoutRequest);
-UsersRouter.post("/sign-up", passportController.encryptPassword);
 UsersRouter.post(
-  "/log-in",
+  "/sign-up",
   body("password")
     .isLength({ min: 5 })
     .withMessage("Password must be at least 5 characters long"),
@@ -35,6 +34,32 @@ UsersRouter.post(
     }
     next();
   },
+  passportController.encryptPassword
+);
+UsersRouter.post(
+  "/log-in",
+  // body("password")
+  //   .isLength({ min: 5 })
+  //   .withMessage("Password must be at least 5 characters long"),
+  // body("confirm-password").custom((value, { req }) => {
+  //   if (value !== req.body.password) {
+  //     throw new Error("Password confirmation does not match password");
+  //   }
+  //   return true;
+  // }),
+  // (req, res, next) => {
+  //   const errors = validationResult(req);
+  //   if (!errors.isEmpty()) {
+  //     const errorMap = errors.array().reduce((acc, error) => {
+  //       acc[error.path] = error.msg;
+  //       return acc;
+  //     }, {});
+  //     res.status(404).render("failure", {
+  //       errors: Object.values(errorMap)
+  //     });
+  //   }
+  //   next();
+  // },
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/failure",
