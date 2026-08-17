e9226ef900776d78d1912a1e5adc13038fc628c1
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-17 17:39:34 -0400

    added code for dropdown menu animations

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,16 @@
 const dropdownBtn = document.getElementById("dropdownBtn");
 const dropdownMenu = document.getElementById("dropdownMenu");
+const dropdownIcon = document.getElementById("dropdownIcon");
 
 dropdownBtn.addEventListener("click", (e) => {
   e.stopPropagation();
-  dropdownMenu.classList.toggle("hidden");
+  dropdownMenu.classList.toggle("opacity-0");
+  dropdownMenu.classList.toggle("scale-y-0");
+  dropdownMenu.classList.toggle("pointer-events-none");
+
+  dropdownMenu.classList.toggle("opacity-100");
+  dropdownMenu.classList.toggle("scale-y-100");
+  dropdownMenu.classList.toggle("pointer-events-auto");
+
+  dropdownIcon.classList.toggle("rotate-360");
 });
