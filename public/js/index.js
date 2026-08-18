7cae73b308743c6846fafa1113c62f517051468b
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-18 15:06:27 -0400

    added eventlisteners to modal buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
 const dropdownBtn = document.getElementById("dropdownBtn");
 const dropdownMenu = document.getElementById("dropdownMenu");
 const dropdownIcon = document.getElementById("dropdownIcon");
+const modalBtn = document.getElementById("modalBtn");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("close-modal-btn");
 
 dropdownBtn.addEventListener("click", (e) => {
   e.stopPropagation();
@@ -13,4 +16,27 @@ dropdownBtn.addEventListener("click", (e) => {
   dropdownMenu.classList.toggle("pointer-events-auto");
 
   dropdownIcon.classList.toggle("rotate-360");
+});
+
+modalBtn.addEventListener("click", () => {
+  modal.showModal();
+  console.log("hello?");
+});
+
+// close modal
+closeModalBtn.addEventListener("click", () => {
+  modal.close();
+});
+
+// if user clicks on the backdrop area outside of modal the modal closes
+modal.addEventListener("click", (e) => {
+  const dialogDimensions = modal.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    modal.close();
+  }
 });
