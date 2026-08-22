7e9748da0202b541ae6c0b41950ca9b525eb1d6f
Brian Abidin <Brian.abidin1@gmail.com>
2026-08-21 23:26:17 -0400

    added and exported createNewFile query function for prisma db

--- services/userService.js
+++ services/userService.js
@@ -1,1 +1,18 @@
 const prisma = require("../lib/prisma");
+
+async function createNewFile(name, type, mediaType, path, size, userId) {
+  await prisma.item.create({
+    data: {
+      name,
+      type,
+      mediaType,
+      path,
+      size,
+      userId
+    }
+  });
+}
+
+module.exports = {
+  createNewFile
+};
